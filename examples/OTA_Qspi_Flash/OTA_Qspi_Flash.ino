a0400c027f858754d5cdd1d53022c655c260b4ee
giulcioffi <g.cioffi@arduino.cc>
2020-08-28 11:40:48 +0200

    Add support for QSPI_FLASH_OFFSET ota

---
+++ examples/OTA_Qspi_Flash/OTA_Qspi_Flash.ino
@@ -0,0 +1,52 @@
+/*
+ This example demonstrates how to use the OTA on Portenta, 
+ flashing the binary stored in the Internal Flash in Bank1.
+ With the library ArduinoOTAPortenta it is currently possible to test
+ only the storage type INTERNAL_FLASH_OFFSET. 
+ Before uploading this sketch:
+ Flash OTA_Usage_Portenta.ino.bin through dfu-util at address 0x08080000
+*/
+#include "ArduinoOTAPortenta.h"
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {}
+
+  Serial.println("*****OTA from M4 Flash*****");
+  /*
+  Serial.println("Select the storage mode. Press:");
+  Serial.println("0: raw device, no offset");
+  Serial.println("1: have a filesystem, use offset as partition start");
+
+  while (!Serial.available());
+  int mode = Serial.readStringUntil('\n').toInt();
+  */
+
+  int started = millis();
+  /*
+  if (mode == 0) {
+    Serial.println("OTA from raw flash");
+    OTAPortenta.begin(INTERNAL_FLASH_OFFSET,0x80000);
+  } else if (mode == 1) {
+    Serial.println("OTA from flash with filesystem");
+    //OTAPortenta.begin(InternalFlashOffset);
+  } else {
+    Serial.println("Invalid mode");
+    while(1) {}
+  }
+  */
+  OTAPortenta.setUpdateLen(131728);
+  OTAPortenta.begin(QSPI_FLASH_OFFSET,0);
+
+  pinMode(LEDB, OUTPUT);
+  digitalWrite(LEDB, LOW);
+  delay(5000);
+  digitalWrite(LEDB, HIGH);
+
+  Serial.println("Restarting the system with updated fw...");
+
+}
+
+void loop() {
+  OTAPortenta.update();
+}
--- src/OTAStorage_Portenta_Qspi_Flash.cpp
+++ src/OTAStorage_Portenta_Qspi_Flash.cpp
@@ -32,7 +32,9 @@ using namespace arduino;
    CONSTANTS
  ******************************************************************************/
 
-static char const SD_UPDATE_FILENAME[] = "UPDATE.BIN";
+//mbed::BlockDevice* bd = NULL;
+
+QSPIFBlockDevice qspi_bd;
 
 int update_size_Qspi;
 
@@ -42,19 +44,83 @@ int update_size_Qspi;
 
 bool OTAStorage_Portenta_Qspi_Flash::init()
 {
-  /* Nothing to do */
+  int err;
+
+  if(storagePortenta==QSPI_FLASH_OFFSET) {
+    err = qspi_bd.init();
+    if (err)
+      return false;
+    else
+      return true;
+  } else {
+    Serial1.println("ERROR: storageType not available");
+    return false;
+  }
 }
 
 bool OTAStorage_Portenta_Qspi_Flash::open()
 {
-  /* Nothing to do */
+  if(storagePortenta==QSPI_FLASH_OFFSET) {
+    update_size_Qspi = program_len;  // 2 * 1024 * 1024
+    return true;
+    /*
+    if ((dir_Flash = opendir("/fs")) != NULL) {
+      while ((ent_Flash = readdir(dir_Flash)) != NULL) {
+        if (String(ent_Flash->d_name) == "UPDATE.BIN") {
+            struct stat stat_buf;
+            stat("/fs/UPDATE.BIN", &stat_buf);
+            update_size_Internal_Flash = stat_buf.st_size;
+            return true;
+        }
+      }
+    }
+  } else if (storagePortenta==INTERNAL_FLASH_OFFSET) {
+    update_size_Internal_Flash = 2 * 1024 * 1024;
+    return true;
+  } else if (storagePortenta==INTERNAL_FLASH_LITTLEFS) {
+    if ((dir_Flash = opendir("/little_fs")) != NULL) {
+      while ((ent_Flash = readdir(dir_Flash)) != NULL) {
+        if (String(ent_Flash->d_name) == "UPDATE.BIN") {
+            struct stat stat_buf;
+            stat("/little_fs/UPDATE.BIN", &stat_buf);
+            update_size_Internal_Flash = stat_buf.st_size;
+            return true;
+        }
+      }
+    }*/
+  } else {
+    Serial1.println("ERROR: storageType not available");
+    update_size_Qspi = 0;
+    return false;
+  }
 }
 
 size_t OTAStorage_Portenta_Qspi_Flash::write()
 {
-  /* Nothing to do */
+  if(storagePortenta==QSPI_FLASH_OFFSET) {
+    HAL_RTCEx_BKUPWrite(&RTCHandle, RTC_BKP_DR0, 0x07AA);
+    Serial1.println("OTAStorage_Internal_Flash::write    1");
+    Serial1.print("OTAStorage_Internal_Flash::write    storagePortenta = ");
+    Serial1.println(storagePortenta);
+    delay(200);
+    HAL_RTCEx_BKUPWrite(&RTCHandle, RTC_BKP_DR1, storagePortenta);
+    Serial1.println("OTAStorage_Internal_Flash::write    2");
+    Serial1.print("OTAStorage_Internal_Flash::write    update_size = ");
+    //int update_size_Internal_Flash = 2 * 1024 * 1024;
+    Serial1.println(update_size_Qspi);
+    delay(200);
 
-  return update_size_Qspi;
+    HAL_RTCEx_BKUPWrite(&RTCHandle, RTC_BKP_DR2, data_offset);
+
+    HAL_RTCEx_BKUPWrite(&RTCHandle, RTC_BKP_DR3, update_size_Qspi);
+    Serial1.println("OTAStorage_Internal_Flash::write    3");
+    delay(200);
+
+    return update_size_Qspi;
+  } else {
+    Serial1.println("storageType not implemented yet");
+    return 0;
+  }
 }
 
 void OTAStorage_Portenta_Qspi_Flash::close()
